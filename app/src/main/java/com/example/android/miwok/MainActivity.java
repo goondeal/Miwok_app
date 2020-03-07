473a9325ce51b064d06447659c446a39d205f056
unknown <ahmed.salamony497@gmail.com>
2020-03-07 17:45:54 +0200

    adding the project

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,49 @@
+
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Customize the ActionBar
+        final ActionBar abar = getSupportActionBar();
+        View viewActionBar = getLayoutInflater().inflate(R.layout.abs_layout, null);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(//Center the textview in the ActionBar !
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.CENTER);
+        TextView textviewTitle = (TextView) viewActionBar.findViewById(R.id.actionbar_textview);
+        textviewTitle.setText("Miwok");
+        abar.setCustomView(viewActionBar, params);
+        abar.setDisplayShowCustomEnabled(true);
+        abar.setDisplayShowTitleEnabled(false);
+        abar.setDisplayHomeAsUpEnabled(true);
+        abar.setIcon(R.color.primary_dark_color);
+        abar.setHomeButtonEnabled(true);
+
+        TabLayout tabs = (TabLayout)findViewById(R.id.tabs);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.view_pager);
+
+        CategoryAdapter adapter = new CategoryAdapter(this,getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        tabs.setupWithViewPager(viewPager);
+
+        }
+    }
